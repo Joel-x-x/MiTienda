679a032ae674fda0eee45b43219b6d1c020b7956
HuesosAce <pauloarielyepez@gmail.com>
2024-01-13 20:06:33 -0500

    fix: fix proveedor database

--- Mi Tienda/src/main/resources/db/migration/V4__create_table_proveedores.sql
+++ Mi Tienda/src/main/resources/db/migration/V4__create_table_proveedores.sql
@@ -2,7 +2,7 @@ create table proveedores (
     id int not null primary key auto_increment,
     identificacion varchar(20),
     razonSocial varchar(100),
-    empresa varchar(),
+    empresa varchar(100),
     direccion varchar(100),
     celular varchar(15),
     correo varchar(50),
