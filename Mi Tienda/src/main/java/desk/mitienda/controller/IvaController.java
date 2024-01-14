2e4e7727a38609d8807433918834be88b964c113
Joel <acostajoel0001@gmail.com>
2024-01-13 20:14:51 -0500

    feat: add controller y dao producto

--- Mi Tienda/src/main/java/desk/mitienda/controller/IvaController.java
+++ Mi Tienda/src/main/java/desk/mitienda/controller/IvaController.java
@@ -1,7 +1,11 @@
 package desk.mitienda.controller;
 
 import desk.mitienda.dao.IvaDao;
+import desk.mitienda.model.Iva;
+import desk.mitienda.utils.Estado;
 import desk.mitienda.utils.JPAUtils;
+
+import java.util.List;
 
 public class IvaController {
 
@@ -11,5 +15,11 @@ public class IvaController {
         ivaDao = new IvaDao(JPAUtils.getEntityManager());
     }
 
+    public Estado actualizar(Iva iva) {
+        return ivaDao.actualizar(iva);
+    }
 
+    public List<Iva> listar() {
+        return ivaDao.listar();
+    }
 }
---
+++ Mi Tienda/src/main/java/desk/mitienda/dao/ProductoDao.java
@@ -0,0 +1,114 @@
+package desk.mitienda.dao;
+
+import desk.mitienda.model.Producto;
+import desk.mitienda.utils.Estado;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class ProductoDao {
+    private EntityManager em;
+    private EntityTransaction transaction;
+
+    public ProductoDao(EntityManager em) {
+        this.em = em;
+        this.transaction = em.getTransaction();
+    }
+
+    public Estado guardar(Producto producto) {
+        try {
+            transaction.begin();
+            this.em.persist(producto);
+            transaction.commit();
+            return new Estado(true, "Producto registrado");
+
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();  // Revierte la transacción si se produce una excepción
+            }
+            e.printStackTrace();
+            return new Estado(false, "No se pudo registrar el producto");
+        } finally {
+            // em.close();
+        }
+    }
+
+    public Estado actualizar(Producto producto) {
+
+        Producto productoExistente;
+
+        try {
+            transaction.begin();
+            // Buscamos producto por id
+            productoExistente = this.em.find(Producto.class, producto.getId());
+            // Pasamos de detached a managed
+            productoExistente = this.em.merge(productoExistente);
+            // Copiamos las propiedades del producto al productoExistente
+            BeanUtils.copyProperties(productoExistente, producto);
+            transaction.commit();
+            return new Estado(true, "Producto actualizado");
+
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();  // Revierte la transacción si se produce una excepción
+            }
+            e.printStackTrace();
+            return new Estado(false, "No se pudo actualizar el producto");
+        } finally {
+            // em.close();
+        }
+    }
+
+    public Estado eliminar(Long id) {
+        Producto producto;
+
+        try {
+            transaction.begin();
+            // Buscamos producto por id
+            producto = this.em.find(Producto.class, id);
+            // Pasamos de detached a managed
+            producto = this.em.merge(producto);
+            // Desactivamos el estado a false
+            producto.desactivar();
+            transaction.commit();
+            return new Estado(true, "Producto eliminado");
+
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();  // Revierte la transacción si se produce una excepción
+            }
+            e.printStackTrace();
+            return new Estado(false, "No se pudo eliminar el producto");
+        } finally {
+            // em.close();
+        }
+    }
+
+    public List<Producto> listar(String codigo, String nombre) {
+        CriteriaBuilder criteriaBuilder =em.getCriteriaBuilder();
+
+        CriteriaQuery<Producto> createQuery = criteriaBuilder.createQuery(Producto.class);
+
+        Root<Producto> from = createQuery.from(Producto.class);
+
+        Predicate filtro = criteriaBuilder.and();
+
+        if(codigo != null && !codigo.trim().isEmpty()) {
+            filtro = criteriaBuilder.and(filtro, criteriaBuilder.like(from.get("codigo"), codigo));
+        }
+
+        if(nombre != null && !nombre.trim().isEmpty()) {
+            filtro = criteriaBuilder.and(filtro, criteriaBuilder.like(from.get("nombre"), nombre));
+        }
+
+        return em.createQuery(createQuery.where(filtro)).getResultList();
+
+    }
+
+}
